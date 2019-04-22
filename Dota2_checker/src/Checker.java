c6c637e5a54089b7177f145cd898b0b5ae660536
AlexLaak <alex.laakkonen@metropolia.fi>
2019-04-22 18:23:26 +0300

    fix not displaying last player

--- Dota2_checker/src/Checker.java
+++ Dota2_checker/src/Checker.java
@@ -104,7 +104,7 @@ public class Checker {
 
 		// Get played with
 
-		for (int j = 0; j < newUrls.size() - 1; j++) {
+		for (int j = 0; j < names.size(); j++) {
 
 			String name = names.get(j);
 			
